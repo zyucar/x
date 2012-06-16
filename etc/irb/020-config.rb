dcf42152226fa65415b2fdd8b4e74448b6d437f6
Recai Oktaş <roktas@bil.omu.edu.tr>
2012-06-16 06:56:55 +0300

    irb: daha idiyomatik

--- etc/irb/020-config.rb
+++ etc/irb/020-config.rb
@@ -5,5 +5,5 @@ Irbtools.add_package :more
 
 # sorunlu gem'leri çıkar
 %w[zucker/debug].each do |m|
-  Irbtools.remove_library 'zucker/debug' rescue ""
+  Irbtools.remove_library 'zucker/debug' rescue nil
 end
