ecae3ae839f8a689f94e15e81cf920aaf128d2de
Recai Oktaş <roktas@bil.omu.edu.tr>
2012-06-19 04:34:41 +0300

    etc: pry yapılandırması ekle

---
+++ etc/pry/170-awesome.rb
@@ -0,0 +1,7 @@
+begin
+  require 'awesome_print'
+  Pry.config.print = proc do |output, value|
+    Pry::Helpers::BaseHelpers.stagger_output("=> #{value.ai}", output)
+  end
+rescue LoadError => err
+end
