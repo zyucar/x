adc9e45ad698786578db019a51a398087edaaca1
Recai Oktaş <roktas@bil.omu.edu.tr>
2014-03-07 16:09:03 +0200

    fish: yapılandırmayı düzelt

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,17 +1,9 @@
-set -l curdir (dirname (status -f))
-
-if not set -q fish_complete_path
-	set fish_complete_path $curdir/completions
-end
+set -lx curdir (dirname (status -f))
 
 if not contains $curdir/completions $fish_complete_path
-	set fish_complete_path[-1] $curdir/completions
-end
-
-if not set -q fish_function_path
-	set fish_function_path $curdir/functions
+	set fish_complete_path $fish_complete_path $curdir/completions
 end
 
 if not contains $curdir/functions $fish_function_path
-	set fish_function_path[-1] $curdir/functions
+ 	set fish_function_path $fish_function_path $curdir/functions
 end
